e27652f14586176bd538c116ebe0d60704aef58c
Vinay Hire <130146950+Vinay0128n@users.noreply.github.com>
2025-10-15 09:04:51 +0530

    Update AddUserForm.jsx
    updated api_url

--- front/src/components/AddUserForm.jsx
+++ front/src/components/AddUserForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://directory-user-backend.onrender.com/api';
 
 const AddUserForm = ({ onUserAdded, onCancel }) => {
   const [formData, setFormData] = useState({
